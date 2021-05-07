b60256b30bb79c4f6d40d2a6619a70b0154e5e8c
Samuel <samuel.morrissey@outlook.com>
2021-05-07 08:44:28 -0300

    Todos colapsáveis e animados corretamente

--- lib/components/todoListComponent.dart
+++ lib/components/todoListComponent.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:todo_flutter/components/todoComponent.dart';
-import 'package:todo_flutter/model/todo.dart';
 import 'package:todo_flutter/model/todoList.dart';
 
 class TodoListComponent extends StatefulWidget {
@@ -12,41 +11,68 @@ class TodoListComponent extends StatefulWidget {
   _TodoListState createState() => _TodoListState();
 }
 
-class _TodoListState extends State<TodoListComponent> {
+class _TodoListState extends State<TodoListComponent>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _animation;
   bool isOpen = false;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200),
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.ease,
+      reverseCurve: Curves.easeOut,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _openClose() {
+    setState(() {
+      isOpen = !isOpen;
+    });
+    if (isOpen) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 2,
-                spreadRadius: 0,
-                color: Colors.grey.shade300,
-                offset: Offset(0, -1),
-              )
-            ],
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(30),
+        GestureDetector(
+          onTap: _openClose,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 2,
+                  spreadRadius: 0,
+                  color: Colors.grey.shade300,
+                  offset: Offset(0, -1),
+                )
+              ],
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(30),
+              ),
             ),
-          ),
-          child: Column(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    if (widget.todoList.todos.length > 0) {
-                      isOpen = !isOpen;
-                      debugPrint(isOpen.toString());
-                    }
-                  });
-                },
-                child: Row(
+            child: Column(
+              children: [
+                Row(
                   children: [
                     Padding(
                       padding: const EdgeInsets.all(18.0),
@@ -60,34 +86,31 @@ class _TodoListState extends State<TodoListComponent> {
                     ),
                   ],
                 ),
-              ),
-              AnimatedContainer(
-                duration: Duration(milliseconds: 1000),
-                curve: Curves.decelerate,
-                height: this.isOpen
-                    ? null
-                    : 0, // FIXME: Sair de null para 0 não tem animação!
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(vertical: 16),
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: widget.todoList.todos.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return TodoComponent(
-                              todo: widget.todoList.todos[index],
-                              color: widget.todoList.color,
-                            );
-                          },
+                SizeTransition(
+                  sizeFactor: _animation,
+                  axisAlignment: -1,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: widget.todoList.todos.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return TodoComponent(
+                                todo: widget.todoList.todos[index],
+                                color: widget.todoList.color,
+                              );
+                            },
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ],
